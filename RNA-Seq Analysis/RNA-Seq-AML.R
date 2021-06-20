25cda7e7fe3c387df8fa9f60dedb157d6e079fe4
nursyahr <nur.sr98@gmail.com>
2021-06-20 13:17:28 +0800

    Updated with gene info

--- RNA-Seq Analysis/RNA-Seq-AML.R
+++ RNA-Seq Analysis/RNA-Seq-AML.R
@@ -1,7 +1,13 @@
-Load libraries
-library(recount)
-library(DESeq2)
-library(dplyr)
+# Load libraries
+suppressMessages( library(recount))
+suppressMessages( library(DESeq2))
+suppressMessages( library(dplyr))
+suppressMessages( library(tibble) )
+suppressMessages( library(ggplot2))
+suppressMessages( library(plotly))
+suppressMessages( library(org.Hs.eg.db) )
+suppressMessages( library( AnnotationDbi ) )
+suppressMessages( library( apeglm) )
 
 
 ### DOWNLOAD AND LOAD DATA
@@ -64,11 +70,77 @@ colData(rse)$group <- sample_info$group
 # Create DEseq dataset: design formula variable of interest is treatment status
 dds <- DESeqDataSet(rse, ~ group)
 
+# Pre-filter to remove low read counts: Make file size smaller and computationally faster to run downstream
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+
+# Specify untreated as the control (ref) group
+colData(dds)$group <- factor(colData(dds)$group, levels = c("untrt","trt"))
+
+# Normalize counts (used for downstream visualization) and save file as .txt
+dds <- estimateSizeFactors(dds)
+normalized_counts <- counts(dds, normalized=TRUE)
+write.table(normalized_counts, file="normalized_counts.txt", sep="\t", quote=F, col.names=NA)
+
+###############################
+#### EXPLORING DE RESULTS I ####
+##############################
+
 # Perform DE analysis
 dds <- DESeq(dds)
 
 # Extract results
-res <- results(dds)
+res <- results(dds, alpha = 0.05)
 
-# Select genes that are significant according to thresholds set previously
-# Get the index position of genes that meet these threshold
+# Shrink LFC
+## Create unshrunk one to compare
+unshrunk_res <- res
+
+# Apply LFC shrink
+res <- lfcShrink(dds, coef = "group_trt_vs_untrt", type = "apeglm")
+
+# MA Plot
+## Shrunk
+plotMA(res, ylim=c(-2,2))
+
+## Unshrunk
+plotMA(unshrunk_res, ylim=c(-2,2))
+
+# Summarize results to see no of genes tested, genes up/down de 
+summary(res, alpha = 0.05)
+
+# Create a tibble of results
+res_tb <- res %>%
+  data.frame() %>%
+  rownames_to_column(var="ensembl_id") %>% 
+  as_tibble()
+
+##################################
+#### OBTAINING SIG GENES LIST ####
+##################################
+
+# Filter acc to thresholds set earlier to obtain list of sig. genes. 
+sigRes <- res_tb %>%
+  filter(res$padj <= pThr & 
+           abs( res$log2FoldChange ) >= logFCThr & 
+           res$baseMean >= baseMeanThr ) %>%
+  # Edit ENSMEBL IDs to remove version (digits after decimal)
+  mutate(ensembl_id= gsub('\\.[0-9]*$', '', ensembl_id))
+
+
+# Creating a tibble of ENSEMBL, ENTREZID, SYMBOL, GENENAME 
+anno <- as_tibble(AnnotationDbi::select(org.Hs.eg.db, sigRes$gene, 
+                              columns=c("ENSEMBL", "ENTREZID", "SYMBOL", "GENENAME"), 
+                              keytype="ENSEMBL"))
+
+# Use <<left_join>> to merge `anno` and `sigRes` 
+sigRes <- sigRes %>%
+  left_join(anno, by = c("ensembl_id" ="ENSEMBL"))
+
+
+
+
+
+
+
+
